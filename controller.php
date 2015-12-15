822a40e009031f975577d2dd873ee5731467319f
Alan Hardman <ahardman@thrivelife.com>
2015-12-15 10:25:51 -0700

    Fix department selector

--- controller.php
+++ controller.php
@@ -9,17 +9,12 @@ namespace Plugin\Teamdaily;
 
 class Controller extends \Controller {
 
-	function __construct($f3) {
-		$f3->set("UI", $f3->get("UI") . ";./app/plugin/teamdaily/view/");
-	}
-
-
 	/**
 	 * Handle HTTP GET request
 	 * @param Base  $f3
 	 * @param array $params
 	 */
-	public function dashboard($f3, $params) {
+	public function dashboard($f3) {
 		$this->_requireLogin();
 		$db = $f3->get("db.instance");
 
@@ -29,14 +24,14 @@ class Controller extends \Controller {
 
 		$start_group = (empty($group_list[0]['id'])) ? $f3->get('td.default.team') : $group_list[0]['id'];
 
-		if(empty($params['id'])) $params['id'] = $start_group;
+		$group_id = $f3->get("GET.group_id") ?: $start_group;
 		if(!$f3->get('GET.date')) {
 			$date = date("Y-m-d", strtotime("Yesterday", \Helper\View::instance()->utc2local()));
 		} else {
 			$date = date("Y-m-d", strtotime($f3->get('GET.date')));
 		}
 		$members = new \Model\Custom("user_group_user");
-		foreach($members->find(array("group_id = ?  AND deleted_date IS NULL", $params['id'])) as $r) {
+		foreach($members->find(array("group_id = ? AND deleted_date IS NULL", $group_id)) as $r) {
 			$team_members[] = $r->user_id;
 		}
 		$team_ids = implode(",", $team_members);
@@ -51,15 +46,14 @@ class Controller extends \Controller {
 				SUM(IF(f.field = 'status' AND f.new_value = 3, 1, 0)) as closed,
 				COUNT(DISTINCT i.id) as late
 			FROM user u
-			LEFT JOIN issue_update_detail d on u.id = d.user_id AND DATE(CONVERT_TZ(d.created_date, 'GMT', '".$f3->get('site.timezone')."')) = '$date'
+			LEFT JOIN issue_update_detail d on u.id = d.user_id AND DATE(CONVERT_TZ(d.created_date, 'GMT', '{$f3->get('site.timezone')}')) = '$date'
 			LEFT JOIN issue_update_field f ON d.id = f.issue_update_id
-			LEFT JOIN issue i on u.id = i.owner_id AND i.due_date = '$date' AND i.closed_date IS NULL AND (i.closed_date IS NULL OR i.due_date < DATE(CONVERT_TZ(i.closed_date, 'GMT', '".$f3->get('site.timezone')."')))
+			LEFT JOIN issue i on u.id = i.owner_id AND i.due_date = '$date' AND i.closed_date IS NULL AND (i.closed_date IS NULL OR i.due_date < DATE(CONVERT_TZ(i.closed_date, 'GMT', '{$f3->get('site.timezone')}')))
 			WHERE u.id IN ($team_ids)  GROUP BY u.id ORDER BY u.name");
 
-
-		//Get the Team user
+		// Get the Team user
 		$user = new \Model\User();
-		$user->load($params['id']);
+		$user->load($group_id);
 		$teamscore = array(
 			'id' =>  $user->id,
 			'name' =>$user->name,
@@ -77,7 +71,7 @@ class Controller extends \Controller {
 		$issue = new \Model\Issue();
 
 		foreach ($scores as &$score) {
-			//Increment Team Scores
+			// Increment Team Scores
 			$teamscore['hours'] 	+= $score['hours'];
 			$teamscore['closed'] 	+= $score['closed'];
 			$teamscore['late'] 	+= $score['late'];
@@ -85,11 +79,9 @@ class Controller extends \Controller {
 			if($score['hours']  >= 6 ) {
 				$points  +=2;
 			}
-
 			if($score['closed']  > 0 ) {
 				$points  +=1;
 			}
-
 			if($score['late']  == 0 ) {
 				$points  +=2;
 			}
@@ -99,28 +91,23 @@ class Controller extends \Controller {
 				$teampoints += 1;
 			} else if($points >= 2) {
 				$score['status'] = 'warning';
-
 			} else {
 				$score['status'] = 'danger';
 			}
 
-			//Load the User's avatar
-
+			// Load the User's avatar
 			$user->load($score['id']);
 			$score['avatar']  = $user->avatar(128);
 
-
-			//Load the user's top issues
+			// Load the user's top issues
 			$score['tasks'] ['Active']= $issue->findone(array("owner_id = ? AND closed_date IS NULL AND deleted_date IS NULL AND status= '2'", $user->id), array('order' => 'due_date, priority DESC'));
 			$score['tasks'] ['Due']= $issue->findone(array("owner_id = ? AND closed_date IS NULL AND deleted_date IS NULL AND date(due_date) < DATE_ADD(NOW(), INTERVAL 2 day)", $user->id), array('order' => 'due_date, priority DESC'));
 			$score['tasks'] ['High']= $issue->findone(array("owner_id = ? AND closed_date IS NULL AND deleted_date IS NULL AND   priority > '0'", $user->id), array('order' => 'priority DESC'));
 			$user->reset();
 
-
-
 		}
 
-		//Calculate team overall success then add team to scores
+		// Calculate team overall success then add team to scores
 		if($teampoints > (count($scores) / 2) ) {
 			$teamscore['status'] = 'success';
 		} else {
@@ -129,13 +116,8 @@ class Controller extends \Controller {
 		$scores[] = $teamscore;
 		$f3->set("results", $scores);
 
-		if(true) {
-
-			$this->_render("td_dashboard.html");
-		} else {
-			$f3->error(404);
-		}
+		// Render page
+		$this->_render("teamdaily/view/td_dashboard.html");
 	}
-
 
 }
